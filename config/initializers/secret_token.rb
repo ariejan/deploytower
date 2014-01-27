fa764f479dc45dba2875684f3350b8521d012c7b
Ariejan de Vroom <ariejan@ariejan.net>
2014-01-27 18:17:50 +0100

    Initial Rails 4.0.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Deploytower::Application.config.secret_key_base = '34c21f53e0b1a07cbf2ad40d2c03e08c79028d083307956f87d2412788e855d499c04acc86c9c3ed5c54bf63ca08f8d79cb50a659cdce57796f3f53ea67a7866'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Deploytower::Application.config.session_store :cookie_store, key: '_deploytower_session'
