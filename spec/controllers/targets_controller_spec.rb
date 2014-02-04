ab54df22117510e5be9ce0f9f21209d0b4ca5790
Ariejan de Vroom <ariejan@ariejan.net>
2014-02-04 21:08:12 +0100

    Use build_stubbed instead of create
    Don't hit that database!

--- spec/controllers/targets_controller_spec.rb
+++ spec/controllers/targets_controller_spec.rb
@@ -2,61 +2,65 @@ require 'spec_helper'
 
 describe TargetsController do
   describe "GET #index" do
-    let(:target) { create :target }
+    let(:target) { build_stubbed :target }
 
     before do
       allow(Target).to receive(:all).and_return([target])
+      get :index
     end
 
     it "assigns an array of targets to @targets" do
-      get :index
       expect(assigns(:targets)).to match_array([target])
     end
 
     it "renders the :index view" do
-      get :index
       expect(response).to render_template(:index)
     end
   end
 
   describe "GET #show" do
-    let(:target) { create :target }
+    let(:target) { build_stubbed :target }
 
     before do
       allow(Target).to receive(:find).and_return(target)
+      get :show, id: target
     end
 
     it "assigns the requested target to @target" do
-      get :show, id: target
       expect(assigns(:target)).to eql(target)
     end
 
     it "renders the :show view" do
-      get :show, id: target
       expect(response).to render_template(:show)
     end
   end
 
   describe "GET #new" do
-    it "assigns a new target to @target" do
+    before do
       get :new
+    end
+
+    it "assigns a new target to @target" do
       expect(assigns(:target)).to be_a_new(Target)
     end
 
     it "render the :new view" do
-      get :new
       expect(response).to render_template(:new)
     end
   end
 
   describe "POST #create" do
-    let(:target) { build :target }
+    let(:target) { build_stubbed :target }
 
     before do
       allow(Target).to receive(:new).and_return(target)
     end
 
     context "with valid attributes" do
+      before do
+        allow(target).to receive(:save).and_return(true)
+      end
+
       it "saves the target to the database" do
         expect(target).to receive(:save)
         post :create, target: attributes_for(:target)
@@ -64,43 +68,45 @@ describe TargetsController do
 
       it "redirects to the target page" do
         post :create, target: attributes_for(:target)
-        expect(response).to redirect_to(Target.last)
+        expect(response).to redirect_to(target)
       end
     end
 
     context "with invalid attributes" do
       before do
         allow(target).to receive(:save).and_return(false)
+        post :create, target: attributes_for(:target)
       end
 
-      it "assigns @target a new record" do
-        post :create, target: attributes_for(:target)
-        expect(assigns(:target)).to be_a_new(Target)
+      it "assigns target to @target" do
+        expect(assigns(:target)).to eql(target)
       end
 
       it "re-renders the :new view" do
-        post :create, target: attributes_for(:target)
         expect(response).to render_template(:new)
       end
     end
   end
 
   describe "GET #edit" do
-    let(:target) { create :target }
+    let(:target) { build_stubbed :target }
+
+    before do
+      allow(Target).to receive(:find).and_return(target)
+      get :edit, id: target
+    end
 
     it "assigns the requested target to @target" do
-      get :edit, id: target
       expect(assigns(:target)).to eql(target)
     end
 
     it "renders the :edit view" do
-      get :edit, id: target
       expect(response).to render_template(:edit)
     end
   end
 
   describe "PUT #update" do
-    let(:target)     { create :target }
+    let(:target)     { build_stubbed :target }
     let(:attributes) { attributes_for(:target) }
 
     before do
@@ -108,6 +114,10 @@ describe TargetsController do
     end
 
     context "with valid attributes" do
+      before do
+        allow(target).to receive(:save).and_return(true)
+      end
+
       it "assigns the target to @target" do
         put :update, id: target, target: attributes
         expect(assigns(:target)).to eql(target)
@@ -142,7 +152,7 @@ describe TargetsController do
   end
 
   describe "DELETE #destroy" do
-    let(:target)     { create :target }
+    let(:target)     { build_stubbed :target }
 
     before do
       allow(Target).to receive(:find).and_return(target)
@@ -154,6 +164,7 @@ describe TargetsController do
     end
 
     it "redirects back to the root url" do
+      allow(target).to receive(:destroy)
       delete :destroy, id: target
       expect(response).to redirect_to("/")
     end
