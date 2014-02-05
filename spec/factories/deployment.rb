9c1dd53b6ce77e976ff46f66036b4c9fab646cff
Ariejan de Vroom <ariejan@ariejan.net>
2014-02-05 20:14:19 +0100

    Add factory for Deployment

--- spec/factories/deployment.rb
+++ spec/factories/deployment.rb
@@ -1,2 +1,13 @@
 FactoryGirl.define do
+  factory :deployment do
+    target
+
+    branch      { "master" }
+    revision    { "69f4f22c4e6509e63542536b83271d4fe620bc1c" }
+
+    started_at  { 90.minutes.ago }
+    finished_at { 88.minutes.ago }
+
+    state       { "finished" }
+  end
 end
