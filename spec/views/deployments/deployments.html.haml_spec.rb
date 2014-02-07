edb6c86d2ae803e76d8b051ab83720d3201537b2
Ariejan de Vroom <ariejan@ariejan.net>
2014-02-07 14:26:03 +0100

    Update styling and info for deployments

--- spec/views/deployments/deployments.html.haml_spec.rb
+++ spec/views/deployments/deployments.html.haml_spec.rb
@@ -15,4 +15,14 @@ describe 'deployment partial' do
   it 'renders the branch name' do
     expect(rendered).to have_css('.branch', text: 'master')
   end
+
+  it 'renders the git commit hash' do
+    expect(rendered).to have_css('.revision',
+      text: '69f4f22c4e6509e63542536b83271d4fe620bc1c')
+  end
+
+  it 'renders the state' do
+    expect(rendered).to have_css('.state',
+      text: 'queued')
+  end
 end
